167fb6f39a6171362429a70ba73303de175766cb
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-04-14 17:42:42 +0100

    rollyboy basics

---
+++ scripts/roller.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+const AGGRO_RANGE = 400
+const BASE_SPEED = 150
+const AGGRO_SPEED = 400
+
+@onready var player: Player = $"../Player"
+@onready var sprite: AnimatedSprite2D = $"AnimatedSprite2D"
+
+var target: Creature
+
+func find_closest_enemy() -> Variant:
+	var closest_enemy = null
+	var closest_distance = INF
+	
+	for enemy in get_tree().get_nodes_in_group("enemy"):
+		var distance = position.distance_squared_to(enemy.position)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_enemy = enemy
+
+	return closest_enemy
+
+func _reset_to_running():
+	target = null
+	sprite.play("default")
+
+func _physics_process(_delta: float) -> void:
+	if player and not target:
+		velocity = (player.position - position).normalized() * BASE_SPEED
+
+	if target and is_instance_valid(target):
+		if position.distance_to(target.position) > AGGRO_RANGE:
+			_reset_to_running()
+		else:
+			velocity = (target.position - position).normalized() * AGGRO_SPEED
+	else:
+		_reset_to_running()
+
+	if velocity.x > 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+
+	move_and_slide()
+
+	var enemy = find_closest_enemy()
+	if enemy and not target and position.distance_to(enemy.position) <= AGGRO_RANGE:
+		target = enemy
+		sprite.play("rolling")
