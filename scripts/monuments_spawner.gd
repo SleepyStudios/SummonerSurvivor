004b3cffcedddb15e1f5718fd13a6a48c19dd393
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-04-15 16:31:02 +0100

    bush and fixes

--- scripts/monuments_spawner.gd
+++ scripts/monuments_spawner.gd
@@ -1,7 +1,7 @@
 class_name MonumentsSpawner extends Node2D
 
 const SPAWN_OFFSET = 128
-const SPAWN_BOUNDARY = 260
+const SPAWN_BOUNDARY = 200
 
 @onready var cam: Camera2D = $"../Player/Camera2D"
 var scene = preload ("res://scenes/world/monument.tscn")
@@ -20,7 +20,7 @@ func spawn() -> void:
       var nodes_to_check = get_children().duplicate()
       nodes_to_check.append_array(get_tree().get_nodes_in_group("monument_spawn_avoid"))
 
-      if not nodes_to_check.any(func (node: Node2D): return node.position.distance_to(rand_pos) <= SPAWN_BOUNDARY):
+      if not nodes_to_check.any(func (node: Node2D): return node.global_position.distance_to(rand_pos) <= SPAWN_BOUNDARY):
         var monument = scene.instantiate()
         monument.position = rand_pos
         add_child(monument)
