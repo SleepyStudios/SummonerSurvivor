fcb71dc2cbb4b85ebfefdae08fc3e0b8ae449546
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-04-15 20:59:10 +0100

    feet hitbox

--- scripts/slime_path.gd
+++ scripts/slime_path.gd
@@ -1,4 +1,4 @@
-extends Creature
+extends Area2D
 
 var amplitude = 0.1
 var period = 2
@@ -15,9 +15,11 @@ func _process(delta):
   scale.y = scale_factor
 
 func _on_timer_timeout() -> void:
-  $Hitbox.does_physical_damage = false
-
   var tween = get_tree().create_tween()
   tween.tween_property($Sprite2D, "modulate", Color.TRANSPARENT, 0.35).set_trans(Tween.TRANS_LINEAR)
   tween.tween_property($Sprite2D, "scale", Vector2.ZERO, 0.45).set_trans(Tween.TRANS_LINEAR)
   tween.tween_callback(queue_free)
+
+func _on_area_entered(area: Area2D) -> void:
+  if area.is_in_group("player_feet"):
+    area.get_parent().hitbox.handle_hit()
