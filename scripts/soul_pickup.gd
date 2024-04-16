f8c93188d163a2e6df310a4d8282df8803f3b734
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-04-16 15:03:08 +0100

    another potential crash fix

--- scripts/soul_pickup.gd
+++ scripts/soul_pickup.gd
@@ -6,18 +6,25 @@ const MAX_SPEED = 360
 @onready var player: Player = $"../Player"
 
 var velocity: Vector2
+var picked_up = false
 
 func _get_speed() -> float:
 	var distance = position.distance_to(player.position)
 	return lerp(MAX_SPEED, 16, clamp(distance / ATTRACTION_DISTANCE, 0, 1))
 
 func _physics_process(_delta: float) -> void:
+	if picked_up:
+		return
+
 	if player:
 		velocity = position.direction_to(player.position) * _get_speed()
 
 	position += velocity * _delta
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if picked_up:
+		return
+
 	if body.is_in_group("player"):
 		player.on_pickup_soul()
 		queue_free()
